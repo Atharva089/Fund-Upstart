1f4279050da1c40b1b1090f289111de15b29f9de
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2023-10-18 01:43:21 +0530

    Modified Ed-Tech page using youtube displayer

--- fundupstart/app/edtech/page.js
+++ fundupstart/app/edtech/page.js
@@ -16,7 +16,7 @@ export default function Edtech() {
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Company ABC <br />
+              EduMentor Prodigy <br />
               <button
                 disabled={true}
                 className="inline-flex text-black bg-gray-200 border-0 py-1 px-3 focus:outline-none rounded text-sm"
@@ -46,120 +46,83 @@ export default function Edtech() {
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <YouTube
-              videoId="sTnm5jvjgjM"
-              opts={opts}
-              //   onReady={this.onReady}
-            />
+            <YouTube videoId="sTnm5jvjgjM" opts={opts} />
           </div>
         </div>
       </section>
-      <section className="text-gray-600 body-font overflow-hidden">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="-my-8 divide-y-2 divide-gray-100">
-            <div className="py-8 flex flex-wrap md:flex-nowrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="font-semibold title-font text-gray-700">
-                  CATEGORY
-                </span>
-                <span className="mt-1 text-gray-500 text-sm">Ed-Tech</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                  ABC
-                </h2>
-                <p className="leading-relaxed">
-                  This is company number 1 in ed Tech startups category and has
-                  a very unique way of teaching students.
-                </p>
-                <a
-                  href="/payments"
-                  className="text-indigo-500 inline-flex items-center mt-4"
-                >
-                  Fund the Idea
-                  <svg
-                    className="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              LearnHub Connect <br />
+              <button
+                disabled={true}
+                className="inline-flex text-black bg-gray-200 border-0 py-1 px-3 focus:outline-none rounded text-sm"
+              >
+                Ed-Tech
+              </button>
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              LearnHub Connect is an EdTech startup dedicated to bridging the
+              gap between educators and students by offering a versatile,
+              virtual learning environment. The platform empowers teachers to
+              create and deliver immersive online courses while providing
+              students with a platform to access high-quality educational
+              content. In addition to live classes and on-demand lessons,
+              LearnHub Connect fosters a vibrant community where students can
+              collaborate and seek help from peers and educators. The startup
+              aims to enhance the e-learning experience and inspire a love for
+              learning in students of all ages.
+            </p>
+            <div className="flex justify-center">
+              <button
+                disabled={true}
+                className="inline-flex text-black bg-indigo-400 border-0 py-1 px-3 font-semibold focus:outline-none hover:bg-indigo-300 rounded text-sm"
+              >
+                <a href="/payments">Fund the Idea</a>
+              </button>
             </div>
-            <div className="py-8 flex flex-wrap md:flex-nowrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="font-semibold title-font text-gray-700">
-                  CATEGORY
-                </span>
-                <span className="mt-1 text-gray-500 text-sm">Ed-Tech</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                  PQR
-                </h2>
-                <p className="leading-relaxed">
-                  We made coaching in India affordable and accessible to all.
-                </p>
-                <a
-                  href="/payments"
-                  className="text-indigo-500 inline-flex items-center mt-4"
-                >
-                  Fund the Idea
-                  <svg
-                    className="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <YouTube videoId="sTnm5jvjgjM" opts={opts} />
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              SkillEdu Pro <br />
+              <button
+                disabled={true}
+                className="inline-flex text-black bg-gray-200 border-0 py-1 px-3 focus:outline-none rounded text-sm"
+              >
+                Ed-Tech
+              </button>
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              SkillEdu Pro is an EdTech startup that specializes in skill-based
+              education and professional development. This platform provides a
+              comprehensive range of courses, certifications, and career
+              resources, empowering learners to acquire practical skills that
+              are highly sought after in today's job market. SkillEdu Pro
+              collaborates with industry experts and leading organizations to
+              ensure that their courses align with real-world demands. The
+              startup is committed to helping learners gain the knowledge and
+              expertise they need to advance their careers and achieve their
+              professional goals.
+            </p>
+            <div className="flex justify-center">
+              <button
+                disabled={true}
+                className="inline-flex text-black bg-indigo-400 border-0 py-1 px-3 font-semibold focus:outline-none hover:bg-indigo-300 rounded text-sm"
+              >
+                <a href="/payments">Fund the Idea</a>
+              </button>
             </div>
-            <div className="py-8 flex flex-wrap md:flex-nowrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="font-semibold title-font text-gray-700">
-                  CATEGORY
-                </span>
-                <span className="mt-1 text-gray-500 text-sm">Ed-Tech</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                  XYZ
-                </h2>
-                <p className="leading-relaxed">
-                  We are the most profitable company in the ed Tech startups.
-                </p>
-                <a
-                  href="/payments"
-                  className="text-indigo-500 inline-flex items-center mt-4"
-                >
-                  Fund the Idea
-                  <svg
-                    className="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-            </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <YouTube videoId="sTnm5jvjgjM" opts={opts} />
           </div>
         </div>
       </section>
