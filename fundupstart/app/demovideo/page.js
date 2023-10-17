fd5fc6257d594406c6cef4b2a046f2796a426b4d
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2023-10-18 01:34:09 +0530

    Youtube Video displayer added

---
+++ fundupstart/app/demovideo/page.js
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+import YouTube from "react-youtube";
+
+export default function page() {
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const onReady = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
+
+  return (
+    <div>
+      <h3>GeeksforGeeks - Youtube</h3>
+      <YouTube
+        videoId="sTnm5jvjgjM"
+        opts={opts}
+        //   onReady={this.onReady}
+      />
+    </div>
+  );
+  //   _onReady(event) {
+  //     event.target.pauseVideo();
+  //   }
+}
--- fundupstart/app/edtech/page.js
+++ fundupstart/app/edtech/page.js
@@ -1,9 +1,59 @@
 "use client";
 import React from "react";
+import YouTube from "react-youtube";
 
 export default function Edtech() {
+  const opts = {
+    height: "300",
+    width: "500",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
     <div>
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              Company ABC <br />
+              <button
+                disabled={true}
+                className="inline-flex text-black bg-gray-200 border-0 py-1 px-3 focus:outline-none rounded text-sm"
+              >
+                Ed-Tech
+              </button>
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              EduMentor Prodigy is an innovative EdTech startup that focuses on
+              personalized learning experiences for K-12 students. By leveraging
+              AI-driven assessments, the platform tailors educational content to
+              individual student needs, ensuring that learners receive the
+              support and challenges they require to excel academically. The
+              startup offers a wide array of interactive lessons, quizzes, and
+              engaging activities across various subjects. EduMentor Prodigy is
+              committed to making quality education accessible to every student,
+              regardless of their geographical location, through an easy-to-use
+              online platform.
+            </p>
+            <div className="flex justify-center">
+              <button
+                disabled={true}
+                className="inline-flex text-black bg-indigo-400 border-0 py-1 px-3 font-semibold focus:outline-none hover:bg-indigo-300 rounded text-sm"
+              >
+                <a href="/payments">Fund the Idea</a>
+              </button>
+            </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <YouTube
+              videoId="sTnm5jvjgjM"
+              opts={opts}
+              //   onReady={this.onReady}
+            />
+          </div>
+        </div>
+      </section>
       <section className="text-gray-600 body-font overflow-hidden">
         <div className="container px-5 py-24 mx-auto">
           <div className="-my-8 divide-y-2 divide-gray-100">
