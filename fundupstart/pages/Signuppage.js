e67dd36601291516af492acc8ee1a6a4c722acb8
Shashwat Tripathi <97380435+Shashwat3012@users.noreply.github.com>
2024-01-16 09:08:40 +0530

    Update Signuppage.js

--- fundupstart/pages/Signuppage.js
+++ fundupstart/pages/Signuppage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Signup from "../components/signup";
